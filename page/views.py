2162966044ed8b536724a5418e2a076008eb1b5d
jbrown400 <justin.brown400@gmail.com>
2019-03-02 12:46:48 -0600

    Initial styling.

--- page/views.py
+++ page/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 
-
-# def index(request):
-#     template = loader.get_template('page/index.html')
-#     return HttpResponse(template.render(request))
 
 def index(request):
     return render(request, 'page/index.html')
