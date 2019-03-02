d292b64e64eea1980b5df170d9de9587a3f4ba6b
jbrown400 <justin.brown400@gmail.com>
2019-03-02 12:13:18 -0600

    Trying to load the index.html file through templates but something isn't working...

--- page/views.py
+++ page/views.py
@@ -1,4 +1,11 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+
+# def index(request):
+#     template = loader.get_template('page/index.html')
+#     return HttpResponse(template.render(request))
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, 'page/index.html')
